9c0120b60d0d7cbf33a31cd37b0e7ca028b67ea1
ropod7 <ropod7@gmail.com>
2016-06-21 19:29:45 +0300

    OOP Structure upgrade

--- sorts.py
+++ sorts.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env/python3
 
 # In this script case using Divide & Conquer paradigm, to:
-# 1. Sort list objects (MergeSort, QuickSort) and
+# 1. Sort list objects and
 # 2. Count number of SplitInversions in unsorted list object
+# by RecursiveTree, MergeSort and QuickSort algorithms
 
 import os
 import random
 import datetime
 
-class MergeSort(object):
+class BaseSort(object):
     sortedOut = []
 
     def __init__(self, obj):
@@ -21,47 +22,19 @@ class MergeSort(object):
         self.sortedOut = self._recursiveSort(self.obj)
         print('executed at:', datetime.datetime.now()-time)
 
-    def _get_value(self, obj, i):
-        return obj[i] if i != len(obj) else self.over
-
-    def _merge(self, a, b, lenl):
-        output = []
-        i = j = 0
-        for k in range(lenl):
-            A = self._get_value(a, i)
-            B = self._get_value(b, j)
-            if A < B:
-                output.append(A)
-                i += 1
-            else:
-                output.append(B)
-                j += 1
-        return output
-
-    def _split(self, obj, n):
-        return obj[:n//2], obj[n//2:]
-
-    def _recursiveSort(self, obj):
-        n = len(obj)
-        if n == 1: return 0
-        a, b = self._split(obj, n)
-        a = self._recursiveSort(a) if len(a) > 1 else a  # recursive call for 1st half
-        b = self._recursiveSort(b) if len(b) > 1 else b  # recursive call for 2nd half
-        d = self._merge(a, b, n)
-        return d
-
     def repr_results(self):
         is_sorted = self.sortedOut == self.sortedIn
         data = 'List is sorted: {0}'.format(is_sorted)
         return data
 
-class SplitInversions(MergeSort):
-    inversions = 0
+class MergeSort(BaseSort):
 
-    def _merge(self, a, b, lenl):
+    def _get_value(self, obj, i):
+        return obj[i] if i != len(obj) else self.over
+
+    def _merge(self, a, b, lenl, si):
         output = []
         i = j = 0
-        len_a = len(a)
         for k in range(lenl):
             A = self._get_value(a, i)
             B = self._get_value(b, j)
@@ -71,31 +44,49 @@ class SplitInversions(MergeSort):
             else:
                 output.append(B)
                 j += 1
-                self.inversions += len_a - i
+                if si: self.inversions += len(a) - i
         return output
+
+    def _split(self, obj, n):
+        return obj[:n//2], obj[n//2:]
+
+    def _recursiveSort(self, obj, si=False):
+        n = len(obj)
+        if n == 1: return 0
+        a, b = self._split(obj, n)
+        a = self._recursiveSort(a) if len(a) > 1 else a  # recursive call for 1st half
+        b = self._recursiveSort(b) if len(b) > 1 else b  # recursive call for 2nd half
+        d = self._merge(a, b, n, si)
+        return d
+
+class SplitInversions(MergeSort):
+    inversions = 0
+
+    def _recursiveSort(self, *args):
+        return super(SplitInversions, self)._recursiveSort(*args, si=True)
 
     def repr_results(self):
         data = super(SplitInversions, self).repr_results()
         data += '\nSplit inversions: {0}'.format(self.inversions)
         return data
 
-class QuickSort(MergeSort):
+class QuickSort(BaseSort):
 
     def _swap(self, item0, item1):
         return item1, item0
 
     def _choose_pivot(self, obj):
         half = len(obj)//2
-        index = random.randrange(0, half+1)     # choosen randomly from 50% of n
-        pivot = obj[index]
-        return index, pivot
+        index = random.randrange(0, half+1)                 # choosen randomly from 50% of n
+        obj[0], obj[index] = self._swap(obj[0], obj[index]) # place pivot to the 0 pos
+        return obj
 
     def _recursiveSort(self, obj):
         self.obj = obj
         n = len(obj)
         if n == 1: return
-        index, pivot = self._choose_pivot(obj)
-        obj[0], obj[index] = self._swap(obj[0], obj[index]) # place pivot to the 0 pos
+        obj = self._choose_pivot(obj)
+        pivot = obj[0]
         obj = self._partitionSort(obj, pivot, n)
         index = obj.index(pivot)                            # get index of current pos of pivot
         a, b = obj[:index], obj[index+1:]                   # split partitions around pivot
@@ -113,7 +104,7 @@ class QuickSort(MergeSort):
         return obj
 
 if __name__ == '__main__':
-    with open('IntegerArray.txt') as f:
+    with open('/media/roman/100GB/Downloads/IntegerArray.txt') as f:
         lines = list(map(int, f.readlines()))
     # or just:
     #lines = [1,5,8,2,6,9,4,7,3,11,14,10,12,13]
