b6b6af6a5cb57d1652048a9dab706e7b69527a9e
ropod7 <ropod7@gmail.com>
2016-07-31 18:50:12 +0300

    BFS and DFS algorithms added

--- graphs.py
+++ graphs.py
@@ -1,24 +1,25 @@
+#!/usr/bin/env/python3
 # -*- coding: utf-8 -*-
-
+# TODO: update algorithms to include explores of different data types
 import os
 import random
+from datetime import datetime
 
 class RContraction(object):                                             # Random Contraction Algorithm
 
     def __init__(self, lines):
-        self.F = 0                                                      # number of edges crossing (A, B)
         self._represent_lines(lines)
 
     def _represent_lines(self, lines):
-        self.n = [int(line[0]) for line in lines]                       # list of vertices
+        self.n = [int(line[0]) for line in lines]                       # generate list of vertices
         self.m = m = []
         for line in lines:
             for item in line[1:]:
                 a, b = [int(line[0]), int(item)]
-                if [a, b] in m or [b, a] in m:
-                    continue                                            # exclude duplicates
+                if [a, b] not in m and [b, a] not in m:
+                    m.append([a, b])                                    # generate list of edges
                 else:
-                    m.append([a, b])                                    # list of edges
+                    continue                                            # exclude duplicates
 
     def _contracter(self, n, m):
         index = random.randrange(0, len(m))                             # pick a remaining edge (u,v) uniformly at random
@@ -35,29 +36,93 @@ class RContraction(object):                                             # Random
                                                                         # or return cut represented by final 2 vertices
     def merge(self):
         self.n, self.m = self._contracter(self.n, self.m)
-        self.F = len(self.m)
+        self.F = len(self.m)                                            # number of edges crossing (A, B)
 
     def repr_results(self):
-        data = 'Minimum cuts is: {0}'.format(self.F)
+        data = 'The minimum Graphs crossing edges: {0}'.format(self.F)
         return data
+
+class Graph(object):
+
+    def __init__(self, obj):
+        self.obj = obj
+        self.vertex = False
+        self.explored = False
+        self.connected = []
+
+    def search(self, vertex):
+        self.vertex = vertex
+        connected = self.connected
+        connected.append(vertex)
+        return connected
+
+    def _get_group(self, v):
+        for group in self.obj:
+            if v is group[0]:
+                return group
+
+    def repr_results(self):
+        string = 'Explored nodes: %s \n' % self.connected
+        return string
+
+class BFS(Graph):                                                       # Breadth First Search algorithm
+
+    def search(self, vertex):
+        connected = super(BFS, self).search(vertex)
+        queue = [vertex]
+        self.dist = 0
+        while queue:
+            v = queue.pop(0)
+            group = self._get_group(v)
+            for w in group[1:]:
+                if w not in connected:
+                    # check distance computation
+                    self.dist = 1 if v is vertex else self.dist + 1     # distance starting from 0
+                    connected.append(w)
+                    queue.append(w)
+
+    def exploreConnected(self):
+        self.explored = explored = []
+        self.pieces = pieces = 0
+        for group in self.obj:
+            v = group[0]
+            if v not in explored:
+                self.search(v)
+                explored.extend(sorted(self.connected))
+                pieces += 1
+        self.pieces = pieces
+
+    def repr_results(self):
+        string = super(BFS, self).repr_results()
+        if not self.explored:
+            string += 'Layers: %d \n' % self.dist
+        else:
+            string += 'Graph Connected Components: %d \n' % self.pieces
+        return string
+
+class DFS(Graph):                                                       # Depth First Search algorithm
+
+    def search(self, vertex):
+        connected = super(DFS, self).search(vertex)
+        group = self._get_group(vertex)
+        for w in group[1:]:
+            if w not in connected:
+                self.search(w)
+
+    def repr_results(self):
+        string = super(DFS, self).repr_results()
+        return(string)
 
 if __name__ == '__main__':
     with open('data/Simplegraphs.txt') as f:
-        lines = [line[:-2].split('\t') for line in f.readlines()]
-    # 0 position of each list is a vertex label, that form an edge with each following
-    #lines = [[1, 2, 5, 4], [2, 3, 4, 1], [3, 2, 5], [4, 2, 1, 5], [5, 1, 3, 4],]
-    cuts = 0
-    for i in range(10):
-        rc = RContraction(lines)
-        rc.merge()
-        if rc.F < cuts:
-            cuts = rc.F
-            cedges = rc.m
-            strres = rc.repr_results()
-        elif i == 0:
-            cuts = rc.F
-            cedges = rc.m
-            strres = rc.repr_results()
-
-    print(strres)
-    print(cedges)
+        lines = [list(map(int, line[:-2].split('\t'))) for line in f.readlines()]
+    #0 position of each list is a vertex label, that form an edge with each following
+    #lines = [[1, 2, 5, 4, 6], [2, 3, 4, 1], [3, 2, 5], [4, 2, 1, 5], [5, 1, 3, 4], [6, 1]]
+    # Disconnected graph
+    #lines = [[1, 3, 5], [2, 4], [3, 1, 5], [4, 2], [5, 1, 3, 7, 9], [6, 8, 10], [7, 5, 9], [8, 6, 10], [9, 5, 7], [10, 6, 8]]
+    t = datetime.now()
+    s = DFS(lines)
+    s.search(6)
+    #s.exploreConnected()
+    print(s.repr_results())
+    print(datetime.now() - t)
